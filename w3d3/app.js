8e3b5eaf551fa4d6cc9b304d471398c0084cc461
Francis Bourgouin <netbeuix@gmail.com>
2023-02-22 12:00:24 -0500

    Hello

---
+++ w3d3/app.js
@@ -0,0 +1,75 @@
+const cookieParser = require("cookie-parser");
+const express = require("express"); // Import Express package, Framework to make a simpler server (http)
+const { authenticateUser, getUserByEmail, createUser, sayBob } = require("./users");
+
+const app = express(); // Instantiate an express object for us to use
+const port = 3000; // Store the port value inside the port variable
+
+app.use(cookieParser());
+app.use(express.static("public")); // Static files (css / images)
+app.use(express.urlencoded({ extended: false })); // Parses the body of a form request string in an object
+app.set("view engine", "ejs"); // Set the view engine to EJS
+
+// Cookies
+// Route to POST to /login
+// Users!
+
+app.get("/", (req, res) => {
+  console.log(req);
+  const { email } = req.cookies;
+
+  const { err, user } = getUserByEmail(email);
+
+  if (err) {
+    const templateVars = { name: null, imagePath: null };
+    return res.render("index", templateVars); // Render the template called "index" and sends the resulting HTML
+  }
+
+  const templateVars = { name: user.name, imagePath: user.imagePath };
+  return res.render("index", templateVars);
+});
+
+app.get("/users/:user_email", (req, res) => {
+  const userEmail = req.params.user_email;
+});
+
+app.post("/login", (req, res) => {
+  // const email = req.body.email
+  // const password = req.body.password
+  const { email, password } = req.body;
+
+  const { err, user } = authenticateUser(email, password);
+
+  if (err) {
+    console.log(err);
+    return res.redirect("/");
+  }
+
+  console.log("success");
+  res.cookie("email", user.email);
+  return res.redirect("/");
+});
+
+app.post("/logout", (req, res) => {
+  sayBob();
+  res.clearCookie("email");
+  return res.redirect("/");
+});
+
+app.get("/register", (req, res) => {
+  return res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const { err, user } = createUser(req.body);
+
+  if (err) {
+    console.log(err);
+    return res.redirect("/register");
+  }
+
+  res.cookie("email", user.email);
+  return res.redirect("/");
+});
+
+app.listen(port, () => console.log(`Express server running on port ${port}`)); // Start listening to requests on port specified by the port variable
---
+++ w3d3/users.js
@@ -0,0 +1,82 @@
+// const urlDatabase = {
+//   "shortUrl":"longUrl"
+// }
+
+// const urlDatabase2 = {
+//   "shortUrl":{
+//     shortUrl:"",
+//     longUrl:"",
+//     userId:""
+//   }
+// }
+
+const user1 = {
+  email: "gentleman@cambrioleur.com",
+  name: "Arsène Lupin",
+  password: "paris",
+  imagePath: "arsene.gif",
+};
+
+const user2 = {
+  email: "pontiac@bandit.com",
+  name: "Doug Judy",
+  password: "rosa",
+  imagePath: "doug.webp",
+};
+
+const userDatabaseOld = {
+  "gentleman@cambrioleur.com": user1,
+  "pontiac@bandit.com": user2,
+};
+const userDatabase = [user1, user2];
+
+const authenticateUser = (email, password) => {
+  // const currentUser = userDatabase[email];
+  const currentUser = userDatabase.find((user) => user.email === email);
+
+  if (!currentUser) {
+    return { err: "User doesn't exist", user: null };
+  }
+
+  if (currentUser.password !== password) {
+    return { err: "Password doesn't match", user: null };
+  }
+
+  return { err: null, user: currentUser };
+  // If exists, check if password is the same
+  // If true, give the user, if not give null
+};
+
+const getUserByEmail = (email) => {
+  // const currentUser = userDatabase[email];
+  const currentUser = userDatabase.find((user) => user.email === email);
+
+  if (!currentUser) {
+    return { err: "User not found", user: null };
+  }
+  return { err: null, user: currentUser };
+};
+
+const createUser = (potentialUser) => {
+  // const currentUser = userDatabase[potentialUser.email];
+  const currentUser = userDatabase.find((user) => user.email === potentialUser.email);
+
+  if (currentUser) {
+    return { err: "User already exists, what are you doing ?!", user: null };
+  }
+
+  for (const key in potentialUser) {
+    const value = potentialUser[key];
+    if (!value) {
+      return { err: `Missing input ${key}`, user: null };
+    }
+  }
+
+  // userDatabase[potentialUser.email] = potentialUser;
+  userDatabase.push(potentialUser);
+  return { err: null, user: potentialUser };
+};
+
+const sayBob = () => console.log("bob");
+
+module.exports = { authenticateUser, getUserByEmail, createUser, sayBob };
